e14965ef8ae104fba9ac6ec108760ed997ce6e36
Volker Strobel <volker.strobel87@gmail.com>
2018-01-12 12:22:32 +0100

    Fixed bug with numbers greater than 10^6

--- extract_occurrences.py
+++ extract_occurrences.py
@@ -24,7 +24,9 @@ def get_num_results(search_term, start_date, end_date):
     div_results = soup.find("div", {"id": "gs_ab_md"}) # find line 'About x results (y sec)
 
     if div_results != None:
-        res = re.findall(r'(\d+),?(\d+)?\s', div_results.text) # extract number of search results
+
+        res = re.findall(r'(\d+),?(\d+)?,?(\d+)?\s', div_results.text) # extract number of search results
+        
         if res == []:
             num_results = '0'
             success = True
